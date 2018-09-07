1b9e106735df6d43e05b75f6fb1b614d7ee027da
alisa <alisa.dammer@gmail.com>
2018-09-08 00:25:26 +0200

    added reting endpoint

--- glass_ship/api.py
+++ glass_ship/api.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask import jsonify, request
 from glass_ship.ship import vesseltrackerservice
-from glass_ship.storage.models import Seafarer
+from glass_ship.storage.models import Seafarer, Report
 from glass_ship.storage.db import init_db, engine
 from sqlalchemy.orm import sessionmaker
 from glass_ship.storage import models
+import datetime
 
 app = Flask("glass-ship")
 app.debug = True
@@ -37,8 +38,29 @@ def store_user():
         session.add(user)
         session.commit()
     else:
-        return jsonify({"Message": "User already exists"}), 200
+        return jsonify({"Message": "User already exists"}), 400
     return jsonify({"Message": "Saved user!"}), 200
+
+
+@app.route('/rate', methods=['POST'])
+def save_rating():
+    """
+    Save rating for a user
+    :return:  response
+    """
+    data = request.get_json()
+    rating = Report(timestamp=datetime.datetime.today(),
+                    device_id=data['device_id'],
+                    user_name=data['name'],
+                    ship_name=data['ship_name'],
+                    food=int(data['food']),
+                    water=int(data['water']),
+                    bedding=int(data['bedding']),
+                    health=int(data['health']),
+                    wage=int(data['wage']))
+    session.add(rating)
+    session.commit()
+    return jsonify({"Message": "Added new rating"})
 
 
 @app.route('/insert_ships', methods=['GET'])
