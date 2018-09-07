536408f79b43894ec39c4868c01b5885d93403d0
oysteiae <oysteiae@stud.ntnu.no>
2018-09-08 00:33:51 +0200

    wip

--- glass_ship/api.py
+++ glass_ship/api.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask import jsonify
 from glass_ship.ship import vesseltrackerservice
 from glass_ship.storage.db import init_db, engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Query
 from glass_ship.storage import models
-
+from glass_ship.helpers import vessel_parsing_helper
 
 app = Flask("glass-ship")
 app.debug = True
@@ -14,12 +14,13 @@ session = Session()
 
 @app.route('/get_ships_names')
 def get_ship_names():
-    vessels = models.Vessel.query.all()
-    vessel_list = []
-    for vessel in vessels:
-        vessel_list.add(vessel.name)
+    vessels = vessel_parsing_helper.get_vessel_name_list(models.Vessel.query.all())
+    return jsonify({"name": vessels}), 200
 
-    return jsonify({"name": vessel_list}), 200
+@app.route('/get_ships_names_by_flag')
+def get_all_ships_by_flag(flag):
+    vessels = vessel_parsing_helper.get_vessel_name_list(session.query(models.Vessel).filter(models.Vessel.flag == flag))
+    return jsonify({"name": vessels}), 200
 
 @app.route('/insert_ships', methods=['GET'])
 def insert_ships():
--- glass_ship/storage/db.py
+++ glass_ship/storage/db.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('postgresql://localhost/glassship')
+engine = create_engine('postgresql://localhost/postgres')
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
