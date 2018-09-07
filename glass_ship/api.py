870ec223692aa54e507c5e55298737a7b87531e0
Tereza Iofciu <t.iofciu@Terezas-MacBook-Pro.local>
2018-09-08 00:36:41 +0200

    added distress

--- glass_ship/api.py
+++ glass_ship/api.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask import jsonify, request
 from glass_ship.ship import vesseltrackerservice
-from glass_ship.storage.models import Seafarer
+from glass_ship.storage.models import Seafarer, Distress
 from glass_ship.storage.db import init_db, engine
 from sqlalchemy.orm import sessionmaker
 from glass_ship.storage import models
+import datetime
 
 app = Flask("glass-ship")
 app.debug = True
@@ -39,6 +40,23 @@ def store_user():
     else:
         return jsonify({"Message": "User already exists"}), 200
     return jsonify({"Message": "Saved user!"}), 200
+
+
+@app.route('/add_distress', methods=['POST'])
+def store_distress():
+    """
+    Store user distress call into db, it can be of three types: injury, abandon, missing person
+    :return: json response
+    """
+    data = request.get_json()
+    distress = Distress(timestamp=datetime.datetime.today(), user_name=data['name'], ship_name=data['ship_name'],
+                        distress_type=data['distress_type'])
+    if not session.query(Seafarer).filter(Seafarer.name == data['name']).first():
+        return jsonify({"Message":"User is not logged in"}), 400
+    
+    session.add(distress)
+    session.commit()    
+    return jsonify({"Message":"Saved distress call"})
 
 
 @app.route('/insert_ships', methods=['GET'])
